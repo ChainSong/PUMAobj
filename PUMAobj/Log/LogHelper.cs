009d848232b412550991cea7632d1e48b697b543
scm2hh@outlook.com <scm2hh@outlook.com>
2020-12-23 14:03:07 +0800

    11

---
+++ PUMAobj/Log/LogHelper.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+[assembly: log4net.Config.XmlConfigurator(ConfigFileExtension = "config", Watch = true)]
+
+
+namespace PUMAobj.Log
+{
+    public class LogHelper
+    {
+        public enum LogLevel { FATAL = 1, Error = 2, Warn = 3, INFO = 4 }
+
+        /// <summary>
+        /// 输出日志到Log4Net
+        /// </summary>
+        /// <param name="t"></param>
+        /// <param name="ex"></param>
+        public static void WriteLog(Type t, Exception ex)
+        {
+            log4net.ILog log = log4net.LogManager.GetLogger(t);
+            log.Error("Error", ex);
+        }
+
+
+        /// <summary>
+        /// 输出日志到Log4Net
+        /// </summary>
+        /// <param name="t"></param>
+        /// <param name="msg"></param>
+        public static void WriteLog(Type t, string msg, LogLevel level)
+        {
+            log4net.ILog log = log4net.LogManager.GetLogger(t);
+            switch (level.ToString())
+            {
+                case "FATAL":
+                    log.Fatal(msg);
+                    break;
+                case "Error":
+                    log.Error(msg);
+                    break;
+                case "Warn":
+                    log.Warn(msg);
+                    break;
+                case "INFO":
+                    log.Info(msg);
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
---
+++ PUMAobj/Log/LogModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PUMAobj.Log
+{
+    public class LogModel
+    {
+        public string SourceFileName { get; set; }
+        public string ToFileName { get; set; }
+        public string Type { get; set; }
+        public string Flag { get; set; }
+        public string ResultDesc { get; set; }
+        public string Externumber { get; set; }
+        
+        
+    }
+}
